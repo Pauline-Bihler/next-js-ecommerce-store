12ae8d7e616704f9ec314a96b92629df0ab59151
Pauline Bihler <pauline.erika5@gmail.com>
2023-10-07 10:23:06 +0200

    Edit .env and git ignore config

--- database.mjs
+++ database.mjs
@@ -1,7 +1,7 @@
 import postgres from 'postgres';
 
 const sql = postgres(
-  'postgres://next_js_ecommerce_store:next_js_ecommerce_store@localhost/next_js_ecommerce_store',
+  'postgres://next_js_ecommerce_store:next_js_ecommerce_store@localhost:5432/next_js_ecommerce_store',
 );
 console.log(
   await sql`
